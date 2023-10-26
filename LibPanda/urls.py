af64cb9d20a901e20194d274f83226e7e2b9557b
unknown <michaelmarcellino54@gmail.com>
2023-10-26 23:10:03 +0700

    setup awal

--- LibPanda/urls.py
+++ LibPanda/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/books", include("book.urls")),
+    path('', include('shoppingcart.urls')),
 ]
