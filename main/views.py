4c81f3f950508f56ea17691d99cba51bda3c3776
Darryl Abysha <daredarryl2010@gmail.com>
2023-10-28 00:12:35 +0700

    Push Landing Page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from book.models import Book
+from random import sample
 
 # Create your views here.
+def show_main(request):
+    # Get all books
+    books = Book.objects.all()
+
+    # Randomly shuffle the books
+    shuffled_books = sample(list(books), len(books))
+
+    for book in shuffled_books:
+        book.formatted_price = "Rp {:,.0f}".format(book.price)
+
+    context = {
+        'books': shuffled_books
+    }
+
+    return render(request, "main.html", context)
