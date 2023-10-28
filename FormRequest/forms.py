13cf43469e723d32fe7e45fcaa5df9d3eb6b6876
nyasiaay <nyasia.aluda@ui.ac.id>
2023-10-28 12:21:07 +0700

    Modif form into AJAX

--- FormRequest/forms.py
+++ FormRequest/forms.py
@@ -2,6 +2,27 @@ from django import forms
 from .models import Book
 
 class BookForm(forms.ModelForm):
+    CATEGORY_CHOICES = (
+        ('Fiksi', 'Fiksi'),
+        ('Sejarah', 'Sejarah'),
+        ('Makanan & Minuman', 'Makanan & Minuman'),
+        ('Komik & Novel Grafis', 'Komik & Novel Grafis'),
+        ('Filosofi', 'Filosofi'),
+    )
+
+    category = forms.ChoiceField(choices=CATEGORY_CHOICES, widget=forms.Select(attrs={'id': 'id_category'}))
+
     class Meta:
         model = Book
         fields = ['title', 'author', 'category', 'year']
+        widgets = {
+            'title': forms.TextInput(attrs={'id': 'id_title'}),
+            'author': forms.TextInput(attrs={'id': 'id_author'}),
+            'year': forms.TextInput(attrs={'id': 'id_year'}),
+        }
+        labels = {
+            'title': 'Judul Buku',
+            'author': 'Penulis',
+            'category': 'Kategori',
+            'year': 'Tahun Terbit',
+        }
