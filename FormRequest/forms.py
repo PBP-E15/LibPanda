a492045f84fd2a49e05643c2ab9422a29cebc56a
nyasiaay <nyasia.aluda@ui.ac.id>
2023-10-28 12:41:31 +0700

    Add validation to the form

--- FormRequest/forms.py
+++ FormRequest/forms.py
@@ -3,6 +3,7 @@ from .models import Book
 
 class BookForm(forms.ModelForm):
     CATEGORY_CHOICES = (
+        ('', 'Pilih kategori'), 
         ('Fiksi', 'Fiksi'),
         ('Sejarah', 'Sejarah'),
         ('Makanan & Minuman', 'Makanan & Minuman'),
@@ -10,15 +11,18 @@ class BookForm(forms.ModelForm):
         ('Filosofi', 'Filosofi'),
     )
 
-    category = forms.ChoiceField(choices=CATEGORY_CHOICES, widget=forms.Select(attrs={'id': 'id_category'}))
+    category = forms.ChoiceField(
+        choices=CATEGORY_CHOICES,
+        widget=forms.Select(attrs={'id': 'id_category'})
+    )
 
     class Meta:
         model = Book
         fields = ['title', 'author', 'category', 'year']
         widgets = {
-            'title': forms.TextInput(attrs={'id': 'id_title'}),
-            'author': forms.TextInput(attrs={'id': 'id_author'}),
-            'year': forms.TextInput(attrs={'id': 'id_year'}),
+            'title': forms.TextInput(attrs={'id': 'id_title', 'placeholder': 'Masukkan judul buku'}),
+            'author': forms.TextInput(attrs={'id': 'id_author', 'placeholder': 'Masukkan nama penulis'}),
+            'year': forms.NumberInput(attrs={'id': 'id_year', 'placeholder': 'Masukkan tahun terbit'}),
         }
         labels = {
             'title': 'Judul Buku',
