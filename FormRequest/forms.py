2f2aa90996d8ec126cb9aa783b6b44135cd92ab7
nyasiaay <nyasia.aluda@ui.ac.id>
2023-10-28 21:56:25 +0700

    Edit style form

--- FormRequest/forms.py
+++ FormRequest/forms.py
@@ -3,7 +3,7 @@ from .models import Book
 
 class BookForm(forms.ModelForm):
     CATEGORY_CHOICES = (
-        ('', 'Pilih kategori'), 
+        ('', 'Pilih kategori'),  
         ('Fiksi', 'Fiksi'),
         ('Sejarah', 'Sejarah'),
         ('Makanan & Minuman', 'Makanan & Minuman'),
@@ -11,22 +11,30 @@ class BookForm(forms.ModelForm):
         ('Filosofi', 'Filosofi'),
     )
 
+    title = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'id': 'id_title', 'placeholder': 'Masukkan judul buku', 'class': 'form-control'}),
+        required=True,
+        label='Judul Buku'  
+    )
+    author = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'id': 'id_author', 'placeholder': 'Masukkan nama penulis', 'class': 'form-control'}),
+        required=True,
+        label='Penulis' 
+    )
     category = forms.ChoiceField(
         choices=CATEGORY_CHOICES,
-        widget=forms.Select(attrs={'id': 'id_category'})
+        widget=forms.Select(attrs={'id': 'id_category', 'placeholder': 'Pilih kategori', 'class': 'form-control'}),
+        required=True,
+        label='Kategori'  
+    )
+    year = forms.IntegerField(
+        widget=forms.NumberInput(attrs={'id': 'id_year', 'placeholder': 'Masukkan tahun terbit', 'class': 'form-control'}),
+        required=True,
+        label='Tahun Terbit'  
     )
 
     class Meta:
         model = Book
         fields = ['title', 'author', 'category', 'year']
-        widgets = {
-            'title': forms.TextInput(attrs={'id': 'id_title', 'placeholder': 'Masukkan judul buku'}),
-            'author': forms.TextInput(attrs={'id': 'id_author', 'placeholder': 'Masukkan nama penulis'}),
-            'year': forms.NumberInput(attrs={'id': 'id_year', 'placeholder': 'Masukkan tahun terbit'}),
-        }
-        labels = {
-            'title': 'Judul Buku',
-            'author': 'Penulis',
-            'category': 'Kategori',
-            'year': 'Tahun Terbit',
-        }
