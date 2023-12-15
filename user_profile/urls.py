43fbfc7e46fb2ca225c317ee645c9e3f4d124253
topahilangharapan <musthofa.joko@ui.ac.id>
2023-12-15 18:25:58 +0700

    fix: missing function

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from user_profile.views import show_profile, edit_biodata, topup_wallet,get_biodata_json,get_wallet_json, get_biodata_flutter, get_wallet_flutter
+from user_profile.views import show_profile, edit_biodata, topup_wallet,get_biodata_json,get_wallet_json, get_biodata_flutter, get_wallet_flutter, edit_biodata_flutter, topup_wallet_flutter
 from user_profile.models import Biodata, Wallet
 
 app_name = 'user_profile'
@@ -12,4 +12,6 @@ urlpatterns = [
     path('get-wallet/',get_wallet_json, name='get_wallet_json'),
     path('get-biodata-flutter/<int:id>', get_biodata_flutter, name='get_biodata_flutter'),
     path('get-wallet-flutter/<int:id>', get_wallet_flutter, name='get_wallet_flutter'),
+    path('edit-biodata-flutter/<int:id>', edit_biodata_flutter, name='edit_biodata_flutter'),
+    path('topup-wallet-flutter/<int:id>', topup_wallet_flutter, name='topup_wallet_flutter'),
 ]
