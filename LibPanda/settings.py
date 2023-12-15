15ecd6dea361ab32970978f68af5f7c8353ec2b9
Darryl Abysha <daredarryl2010@gmail.com>
2023-12-15 15:41:44 +0700

    Redeploy untuk Tugas Akhir v3

--- LibPanda/settings.py
+++ LibPanda/settings.py
@@ -146,3 +146,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["https://libpanda-e15-tk.pbp.cs.ui.ac.id"]
